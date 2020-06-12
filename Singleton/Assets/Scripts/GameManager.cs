e29f094a052477efa56929a53dc283633b3e53b9
Toukiloglou <toukiloglou@gmail.com>
2020-06-12 16:33:47 +0300

    Generic class
    Simple generic singleton class

---
+++ Singleton/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    public override void Init() {
+        base.Init();
+    }
+
+
+    public void Message() {
+        Debug.Log("Hello World");
+    }
+}
